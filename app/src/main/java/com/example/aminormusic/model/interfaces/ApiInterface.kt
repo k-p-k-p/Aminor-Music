772ebf2a51f25b99fff6bbd7a1689dd4eacf88ea
edgysloth <subhamkiran56@gmail.com>
2024-08-03 12:02:12 +0530

    simple api call

--- app/src/main/java/com/example/aminormusic/model/interfaces/ApiInterface.kt
+++ app/src/main/java/com/example/aminormusic/model/interfaces/ApiInterface.kt
@@ -1,6 +1,7 @@
 package com.example.aminormusic.model.interfaces
 
 import com.example.aminormusic.model.apidataclasses.ApiData
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -8,8 +9,7 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @Headers("x-rapidapi-key: RAPID_API_KEY" ,
-        "x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com")
+    @Headers("x-rapidapi-key: 3d6178deb1mshf75399de754f8d6p1767fejsn859830a9af27" , "x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com")
     @GET("search")
     fun getData(@Query("q")query: String): retrofit2.Call<ApiData>
 }
